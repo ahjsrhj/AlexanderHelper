cdb6cf9505afa42d12e7f09dfe63504f9d44cf56
Hexer <renhongjian@accesscorporate.com.cn>
2021-01-28 18:18:41 +0800

    fix bug

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   // publicPath:
   //   process.env.NODE_ENV === 'production'
   // ? 'https://cdn.jsdelivr.net/gh/ahjsrhj/AlexanderHelper@gh-pages/'
-  // : '/',
+  // : './',
+  publicPath: './',
   outputDir: 'dist',
 }
